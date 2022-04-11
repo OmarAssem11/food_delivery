54a1986409a7e4bdb1e02062c4d80fc9ede5707a
hager khaled <hagerk720@gmail.com>
2022-04-11 13:19:31 +0200

    cubit in restaurant screen

--- lib/features/restaurants/presentation/screens/restaurants_list_screen.dart
+++ lib/features/restaurants/presentation/screens/restaurants_list_screen.dart
@@ -1,23 +1,55 @@
 import 'package:flutter/material.dart ';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:food_delivery/core/presentation/widgets/custom_app_bar.dart';
+import 'package:food_delivery/features/restaurants/presentation/bloc/restaurants_cubit.dart';
+import 'package:food_delivery/features/restaurants/presentation/bloc/restaurants_state.dart';
 import 'package:food_delivery/features/restaurants/presentation/widgets/restaurant_item.dart';
 
-class RestaurantsListScreen extends StatelessWidget {
+class RestaurantsListScreen extends StatefulWidget {
   const RestaurantsListScreen();
 
   static const routeName = 'restaurants_list';
 
   @override
+  State<RestaurantsListScreen> createState() => _RestaurantsListScreenState();
+}
+
+class _RestaurantsListScreenState extends State<RestaurantsListScreen> {
+  @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<RestaurantsCubit>(context).getRestaurantsList();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
-      body: ListView.separated(
-            separatorBuilder: (BuildContext context, int index) =>
-                const Divider(thickness: 1, color: Colors.black),
-            itemCount: 8, 
-            itemBuilder: (BuildContext context, int index) {
-              return RestaurantItem(restaurantEntity: null,);
-            }),
+      body: BlocBuilder<RestaurantsCubit, RestaurantsState>(
+        builder: (context, state) {
+          return state.maybeWhen(
+            getAllRestaurantLoading: () {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+            getAllRestaurantSuccess: (restaurants) {
+              return ListView.separated(
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const Divider(thickness: 1, color: Colors.black),
+                  itemCount: 8,
+                  itemBuilder: (BuildContext context, int index) {
+                    return RestaurantItem(
+                      restaurantEntity: restaurants[index],
+                    );
+                  });
+            },
+            orElse: () {
+              return Container();
+            },
+          );
+        },
+      ),
     );
   }
 }
